ffcf9aac5c4e172d9b0ea5bfd4268dcab3e906aa
nhutHao02 <trannhuthao091@gmail.com>
2024-10-30 09:07:25 +0700

    restructuring system

---
+++ internal/api/http/http_server.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/nhutHao02/social-network-tweet-service/config"
+	v1 "github.com/nhutHao02/social-network-tweet-service/internal/api/http/v1"
+)
+
+type HTTPServer struct {
+	cfg *config.Config
+	// handlers
+	// example addressHandler at V1
+}
+
+func NewHTTPServer(cfg *config.Config) *HTTPServer {
+	return &HTTPServer{cfg: cfg}
+}
+
+func (s *HTTPServer) RunHTTPServer() {
+	r := gin.Default()
+	v1.MapRoutes(r)
+	r.Run(s.cfg.HTTPServer.Address)
+}
--- startup/startup.go
+++ startup/startup.go
@@ -1,27 +1,29 @@
 package startup
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 	"github.com/nhutHao02/social-network-tweet-service/config"
+	"github.com/nhutHao02/social-network-tweet-service/internal/api"
+	"github.com/nhutHao02/social-network-tweet-service/internal/api/http"
 )
 
-func StartServer() {
+func Start() {
 	// load congig
 	cfg := config.LoadConfig()
 
 	// database setup
 
-	// setup route
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	// server
+	http_server := http.NewHTTPServer(cfg)
 
-	// setup server
-	r.Run(cfg.HTTPServer.Address)
+	server := api.NewSerVer(http_server)
+	runServer(server)
 
+}
+
+func runServer(server *api.Server) {
+
+	// run http server
+	server.HTTPServer.RunHTTPServer()
+
+	// run grpc server
 }
